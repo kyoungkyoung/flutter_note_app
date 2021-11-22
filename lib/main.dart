fd5603747b520a2c3ef2c5f43b45dcc6b94f7c9a
kyoungkyoung <wkjang4@gmail.com>
2021-11-22 14:42:20 +0900

    di 설정 깔끔하게 업데이트

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_note_app/data/data_source/local/note_data_source.dart';
-import 'package:flutter_note_app/data/repository/note_repository_impl.dart';
 import 'package:flutter_note_app/di/provider_setup.dart';
-import 'package:flutter_note_app/domain/repository/note_repository.dart';
-import 'package:flutter_note_app/domain/use_case/add_note_use_case.dart';
-import 'package:flutter_note_app/domain/use_case/delete_note_use_case.dart';
-import 'package:flutter_note_app/domain/use_case/get_note_use_case.dart';
-import 'package:flutter_note_app/domain/use_case/get_notes_use_case.dart';
-import 'package:flutter_note_app/domain/use_case/note_use_cases.dart';
-import 'package:flutter_note_app/presentation/add_edit_note/add_edit_note_view_model.dart';
-import 'package:flutter_note_app/presentation/notes/notes_view_model.dart';
 import 'package:provider/provider.dart';
-import 'package:provider/single_child_widget.dart';
-import 'package:sqflite/sqflite.dart';
-
-import 'domain/use_case/update_note_use_case.dart';
 
 void main() async {
   // 플랫폼 채널의 위젯 바인딩을 보장
